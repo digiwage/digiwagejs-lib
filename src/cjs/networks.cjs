b3797bb51158f52eec453d93aea32603b65051b1
digiwage <37214252+digiwage@users.noreply.github.com>
2025-03-29 20:12:36 -0400

    add support for digiwage network

--- src/cjs/networks.cjs
+++ src/cjs/networks.cjs
@@ -1,6 +1,7 @@
 'use strict';
 Object.defineProperty(exports, '__esModule', { value: true });
-exports.testnet = exports.regtest = exports.bitcoin = void 0;
+exports.testnet = exports.regtest = exports.bitcoin = exports.digiwage = void 0;
+
 /**
  * Represents the Bitcoin network configuration.
  */
@@ -39,6 +40,7 @@ exports.bitcoin = {
    */
   wif: 0x80,
 };
+
 /**
  * Represents the regtest network configuration.
  */
@@ -53,6 +55,7 @@ exports.regtest = {
   scriptHash: 0xc4,
   wif: 0xef,
 };
+
 /**
  * Represents the testnet network configuration.
  */
@@ -66,4 +69,43 @@ exports.testnet = {
   pubKeyHash: 0x6f,
   scriptHash: 0xc4,
   wif: 0xef,
+};
+
+/**
+ * Represents the Digiwage network configuration.
+ */
+exports.digiwage = {
+  /**
+   * The message prefix used for signing Digiwage messages.
+   */
+  messagePrefix: '\x18Digiwage Signed Message:\n',
+  /**
+   * Digiwage does not use Bech32.
+   */
+  bech32: null,
+  /**
+   * The BIP32 key prefixes for Digiwage.
+   */
+  bip32: {
+    /**
+     * The public key prefix for BIP32 extended public keys.
+     */
+    public: 0x022D2533,
+    /**
+     * The private key prefix for BIP32 extended private keys.
+     */
+    private: 0x0221312B,
+  },
+  /**
+   * The prefix for Digiwage public key hashes.
+   */
+  pubKeyHash: 0x1e,
+  /**
+   * The prefix for Digiwage script hashes.
+   */
+  scriptHash: 0x5a,
+  /**
+   * The prefix for Digiwage Wallet Import Format (WIF) private keys.
+   */
+  wif: 0x59,
 };
